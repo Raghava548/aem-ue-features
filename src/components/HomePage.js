0acff795b75af13f107a46f38933706722e8d1ce
ColemanTO <ColemanTO@gmail.com>
2025-07-30 07:15:15 -0500

    Added section, column and cards to home page & built

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import NYLLayout from './NYLLayout';
-import NYLHero from './NYLHero';
-import piggybackImage from '../images/piggyback_ride.png';
+import React from "react";
+import NYLLayout from "./NYLLayout";
+import NYLHero from "./NYLHero";
+import NYLSection from './NYLSection';
+import NYLColumns from './NYLColumns';
+import NYLCard from './NYLCard';
+import piggybackImage from "../images/piggyback_ride.png";
 
 const HomePage = () => {
   return (
@@ -13,6 +16,40 @@ const HomePage = () => {
         ctaText="Learn More"
         ctaUrl="/about"
       />
+      <NYLSection backgroundColor="gray" sectionId="features">
+        <NYLColumns
+          columnId="feature-cards"
+          gapSize="large"
+          columns={[
+            <NYLCard
+              cardId="card1"
+              title="Life Insurance"
+              description="Protect your family's future with our comprehensive life insurance options."
+              image="/path/to/image.jpg"
+              ctaText="Learn More"
+              ctaUrl="/insurance"
+            />,
+            <NYLCard
+              cardId="card2"
+              title="Investments"
+              description="Grow your wealth with our expert investment strategies."
+              image="/path/to/image.jpg"
+              ctaText="Explore Options"
+              ctaUrl="/investments"
+              variant="featured"
+            />,
+            <NYLCard
+              cardId="card3"
+              title="Retirement"
+              description="Plan for a secure retirement with our retirement solutions."
+              image="/path/to/image.jpg"
+              ctaText="Start Planning"
+              ctaUrl="/retirement"
+              variant="outline"
+            />,
+          ]}
+        />
+      </NYLSection>
       {/* Add more content sections here */}
     </NYLLayout>
   );
